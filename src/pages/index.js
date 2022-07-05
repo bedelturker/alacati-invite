7e3dca535007ffcbc4c0c0557286a15efc669cf2
bedelturker <bedel.turker@muni.com.tr>
2022-07-06 00:17:40 +0300

    managed to connect firebase and add rsvp forms to the database

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,43 @@
-import React from "react"
+import React, { useState } from "react"
+import { doc, setDoc, Timestamp, getFirestore } from "firebase/firestore"
+import db from "../components/firebase"
+
 
 export default function Home() {
-  return <div style={{fontSize: "1.5rem"}}>
-    Hello world!
-  </div>
+
+  // Defining the invite response object
+  const [inviteResponse, setInviteResponse] = useState({
+    dateCreated: Timestamp.fromDate(new Date()),
+    guest : {
+      firstName: "Bedel",
+      lastName: "Türker"
+    },
+    notes: "Vejeteryanız.",
+    hasPlusOne: true,
+    plusOne : {
+      firstName: "Ayşe",
+      lastName: "Obalı"
+    },
+    willAttend: true,
+})
+
+  const rsvpTitle = `${inviteResponse.guest.firstName} ${inviteResponse.guest.lastName}\ 
+  - ${inviteResponse.hasPlusOne ? 2 : 1} Guests`
+
+  const addInvite = async () => {
+    // Add a new form in rsvp collection
+    await setDoc(doc(db, "rsvp", rsvpTitle), inviteResponse)
+    .then(res => console.log('res'))
+    .catch(err => console.log(err))
+  };
+
+  return (
+    <div>
+      <h1>This is a form</h1>
+      <h5>Some text</h5>
+      <button type="submit" className="btn btn-primary" onClick={addInvite}>
+        Add Bedel to List
+      </button>
+    </div>
+  )
 }
