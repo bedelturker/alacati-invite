6e387c3baa90c9d8ea017a304be7983a3391a346
bedelturker <bedel.turker@muni.com.tr>
2022-08-07 17:39:05 +0300

    Added a text to music box.

--- src/components/Utils/MusicBox/MusicBox.js
+++ src/components/Utils/MusicBox/MusicBox.js
@@ -4,7 +4,9 @@ import React, { useState, useEffect, useRef } from "react";
 import speakerIcon from "../../../assets/Icons/volume-solid.svg";
 import speakerSlashIcon from "../../../assets/Icons/volume-slash-solid.svg";
 
+// Media
 import music from "../../../assets/Music/bg-music-default.mp3";
+import arrowDown from "../../../assets/Icons/arrow-down.svg";
 
 const MusicBox = () => {
   const [muted, setMuted] = useState(true);
@@ -12,29 +14,37 @@ const MusicBox = () => {
   const audioRef = useRef();
 
   return (
-    <button className="music_box">
-      <audio
-        id="invite-music"
-        loop
-        autoPlay={true}
-        muted={muted}
-        ref={audioRef}
-      >
-        <source src={music} type="audio/mp3" />
-      </audio>
-      <img
-        className="music_box_image"
-        src={!muted ? speakerIcon : speakerSlashIcon}
-        alt="volume-icon"
-        onClick={() => {
-          setMuted(!muted);
-          const audio = document.getElementById("invite-music");
-          audio.muted = !muted;
-          audio.volume = 0.5;
-          audio.play();
-        }}
+    <div className="music">
+      <span className="music_text">Hit the vibe</span>
+      <img 
+        alt="arrow down"
+        src={arrowDown}
+        className="music_arrow"
       />
-    </button>
+      <button className="music_box">
+        <audio
+          id="invite-music"
+          loop
+          autoPlay={true}
+          muted={muted}
+          ref={audioRef}
+        >
+          <source src={music} type="audio/mp3" />
+        </audio>
+        <img
+          className="music_box_image"
+          src={!muted ? speakerIcon : speakerSlashIcon}
+          alt="volume-icon"
+          onClick={() => {
+            setMuted(!muted);
+            const audio = document.getElementById("invite-music");
+            audio.muted = !muted;
+            audio.volume = 0.5;
+            audio.play();
+          }}
+        />
+      </button>
+    </div>
   );
 };
 
